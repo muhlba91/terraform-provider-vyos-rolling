aee73af7b1624d83a6f1eafba4fef6e67d334e9b
Github Action <noreply@github.com>
2025-12-09 10:56:46 +0800

    feat: Add plan modifier for fwmark attribute in Wireguard resource schema

--- internal/vyos/vyosinterfaces/autogen-netns.go
+++ internal/vyos/vyosinterfaces/autogen-netns.go
@@ -23,7 +23,7 @@ func netns() schemadefinition.InterfaceDefinition {
 				Local: "node",
 			},
 			NodeNameAttr: "netns",
-			OwnerAttr:    "${vyos_conf_scripts_dir}/netns.py",
+			OwnerAttr:    vyosTools.String("${vyos_conf_scripts_dir}/netns.py"),
 			Properties: []*schemadefinition.Properties{{
 				XMLName: xml.Name{
 					Local: "properties",
